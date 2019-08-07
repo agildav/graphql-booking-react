a8a512fe50822a2d891bce1e97fef017483ee5d7
Alberto A. Gil D <agildav@gmail.com>
2019-08-07 18:35:22 -0400

    Add navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import MainNavbar from "../components/navigation/mainNavBar";
 import Auth from "../components/auth/auth";
 import Event from "../components/event/event";
 import Booking from "../components/booking/booking";
@@ -10,12 +11,18 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={Auth} />
-          <Route path="/events" component={Event} />
-          <Route path="/bookings" component={Booking} />
-        </Switch>
+        <div id="App">
+          <MainNavbar />
+          <main id="MainApp">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={Auth} />
+              <Route path="/events" component={Event} />
+              <Route path="/bookings" component={Booking} />
+              <Redirect to="/auth" />
+            </Switch>
+          </main>
+        </div>
       </BrowserRouter>
     );
   }
---
+++ src/components/navigation/mainNavBar.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./mainNavBar.css";
+
+const MainNavBar = () => {
+  return (
+    <header id="MainNavBar">
+      <div className="main-navbar-title main-navbar-logo">
+        <h1>gEvent</h1>
+      </div>
+      <nav className="main-navbar-items">
+        <ul>
+          <NavLink to="/auth">Authenticate</NavLink>
+          <NavLink to="/events">Events</NavLink>
+          <NavLink to="/bookings">Bookings</NavLink>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavBar;
